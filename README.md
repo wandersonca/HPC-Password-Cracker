a5ca970d36a52060e072540878a54e1b0c893515
Will Anderson <willstoney@gmail.com>
2020-11-15 14:24:25 -0800

    good to go...

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # CP631 - Final Project
 [![Build Status](https://cloud.drone.io/api/badges/wandersonca/CP631-FinalProject/status.svg)](https://cloud.drone.io/wandersonca/CP631-FinalProject)
 
+## How to hash a password for testing
+``echo -n 'test' | sha256sum``
 
-## Name: Ma Luo
-Donna Harris was here. These changes were made on the forked DJH version.
+## How to compile
+1. Make sure you install the libssl-dev package. On Ubuntu ``sudo apt-get install libssl-dev``.
+2. Run gcc: ``gcc -std=c99 cli.c -o cli -lcrypto``
+
+## How to compile with Docker
+1. Run gcc within a container: ``docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp gcc:4.9 gcc -std=c99 cli.c -o cli -lcrypto``
+
+## How to run dictionary attack:
+``./cli --dictionary /some/path --password $(echo -n 'test' | sha256sum) --verbose``
+or using short option names:
+``./cli -d /some/path -p $(echo -n 'test' | sha256sum) -v``
+
+## How to run dictionary attack:
+``./cli --bruteforce --lowercase --uppercase --numbers --symbols -p $(echo -n 'test' | sha256sum) --verbose``
+or using short option names:
+``./cli -bluns -p $(echo -n 'test' | sha256sum) -v``
+
+## Authors
+Donna Harris
+Ma Luo
+Will Anderson
---
+++ cli.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "dictionary.c"
+#include "bruteforce.c"
+
+
+void help()
+{
+    printf("WLU Password Cracker Help:\n");
+    printf("--verbose (-v): verbose output\n");
+    //TODO: add the rest of the options
+}
+
+int main(int argc, char **argv)
+{
+    int c;
+    // Flags
+    int verbose_flag = 0;
+    int bruteforce_flag = 0;
+    int dictionary_flag = 0;
+    int character_length = 4;
+    char password_hash[64] = "";
+    char bruteforce_characters[256] = "";
+    char dictionary_path[256] = "";
+
+    while (1)
+    {
+        // Used for parsing long options (--long-option)
+        static struct option long_options[] =
+            {
+                {"verbose", no_argument, 0, 'v'},
+                {"password", required_argument, 0, 'p'},
+                {"dictionary", required_argument, 0, 'd'},
+                {"bruteforce", no_argument, 0, 'b'},
+                {"uppercase", no_argument, 0, 'u'},
+                {"lowercase", no_argument, 0, 'l'},
+                {"numbers", no_argument, 0, 'n'},
+                {"symbols", optional_argument, 0, 's'},
+                {"character-length", required_argument, 0, 'c'}
+            };
+
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long(argc, argv, "vp:d:bulns::c:", long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'v':
+            verbose_flag=1;
+            break;
+        case 'p':
+            strcpy(password_hash,optarg);
+            break;
+        case 'd':
+            dictionary_flag=1;
+            strcpy(dictionary_path,optarg);
+            break;
+        case 'b':
+            bruteforce_flag=1;
+            break;
+        case 'u':
+            strcat(bruteforce_characters, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            break;
+        case 'l':
+            strcat(bruteforce_characters, "abcdefghijklmnopqrstuvwxyz");
+            break;
+        case 'n':
+            strcat(bruteforce_characters, "0123456789");
+            break;
+        case 's':
+            if(optarg == NULL)
+            {
+                strcat(bruteforce_characters, "~!@#$%^&*()_+");
+            } 
+            else 
+            {
+                strcat(bruteforce_characters, optarg);
+            }
+            break;
+        case 'c':
+            strcat(bruteforce_characters, "0123456789");
+            break;
+        case '?':
+            help();
+            break;
+
+        default:
+            abort();
+        }
+    }
+
+    int result;
+
+    // TODO: make these mutually exclusive... 
+    if(dictionary_flag)
+    {
+        result = dictionary_crack(password_hash, dictionary_path, verbose_flag);
+    }
+
+    if(bruteforce_flag) 
+    {
+        result = bruteforce_crack(password_hash, bruteforce_characters, character_length, verbose_flag);
+    }
+    if (result) 
+    {
+        printf("Password not found :(\n");
+    }
+    exit(result);
+}
