0bc3498dc92f3593f7fde72979adadd7bf0a0e64
Donna Harris <donnajharris@gmail.com>
2020-11-22 01:45:37 -0500

    added file reading functionality and comparisons

--- dictionary.c
+++ dictionary.c
@@ -1,31 +1,79 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include "hash.c"
+
+int remove_new_line(char *input, char **output)
+{
+    int len = strlen(input);
+
+    if(input[len-1] == '\n')
+    {
+        *output = (char*)malloc(sizeof(char)*(len-1));
+        strncpy(*output, input, len-1);
+    }
+    else
+    {
+        *output = (char*)malloc(sizeof(char)*(len-1));
+        strncpy(*output, input, len);
+    }
+    
+    return 0;
+}
 
 int dictionary_crack(char* password_hash, char *dictionary_path, int verbose)
 {
-    printf("Using dictionary path: %s\n", dictionary_path);
-    static unsigned char buffer[64];
+    static unsigned char candidate_buffer[64];
+    static unsigned char password_buffer[64];
+
+    FILE *file; 
+    char *line = NULL;
+	char *password_to_test = NULL;
+	size_t len = 0;
+	ssize_t read;
+ 
+	file = fopen(dictionary_path, "r");
+	if (file == NULL)
+		exit(EXIT_FAILURE);
+
+    printf("\n>>> Using dictionary path: %s\n\n", dictionary_path);
+
+    hash(password_hash, password_buffer);
     if(verbose) 
     {
-            printf("Hash to compare: %s\n", password_hash);
+        printf("-----------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+        printf("Looking for this password:\t%16s\t--->\t%s\n", password_hash, password_buffer);
+        printf("-----------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+        printf("\n");
     }
 
-    for(int i=0; i < 1; i++)
+  	while ((read = getline(&line, &len, file)) != -1)
     {
-        // should read in these from a file...
-        char passwordToTest[] = "test";
-        hash(passwordToTest, buffer);
+        remove_new_line(line, &password_to_test);        
+        hash(password_to_test, candidate_buffer);
+
         if(verbose) 
         {
-            printf("`%s` -> %s\n", passwordToTest, buffer);
+            printf("Password candidate from file:\t%16s\t--->\t%s\n", password_to_test, candidate_buffer);
         }
 
-        if (!strcmp(password_hash, buffer))
+//        if (!strcmp(password_hash, buffer))
+        if (!strncmp(password_buffer, candidate_buffer, 64))
         {
-            printf("Password found: %s\n", passwordToTest);
+            printf("\nSUCCESS!!\tPassword found: %s\n", password_to_test);
+            free(password_to_test);
+	        fclose(file);
             return 0;
+            // TODO: What do we really need to return here? Do we need to return the matching value?
         }        
+
+        free(password_to_test);        
     }
+
+    fclose(file);
+
+    printf("\n");
+
     return 1;
 }
