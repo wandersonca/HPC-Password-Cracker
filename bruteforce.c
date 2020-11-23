9524182d4818f7647939fef797e3fe73992e73c0
Will Anderson <willstoney@gmail.com>
2020-11-22 23:45:59 -0500

    wip

--- bruteforce.c
+++ bruteforce.c
@@ -1,18 +1,30 @@
-int bruteforce_crack(char* password_hash, char * characters, int length, int verbose)
+#include <math.h>
+
+int bruteforce_crack(char* password_hash, char * characters, int password_max_length, int verbose)
 {
     printf("Brute force of hash: %s\n",password_hash);
-    printf("Using %d of characters: %s\n", length, characters);
-
+    int number_of_characters = strlen(characters);
+    printf("Using %d characters: %s\n", number_of_characters, characters);
+    long possibilities = (long) pow(number_of_characters, password_max_length);
+    printf("Number of possible words: %d\n", possibilities);
     static unsigned char buffer[65];
     if(verbose) 
     {
             printf("Hash to compare: %s\n", password_hash);
     }
+    char passwordToTest[password_max_length];
 
-    for(int i=0; i < 1; i++)
+    for (int i = 0; i < possibilities; i++)
     {
-        // we should get to this by brute force...
-        char passwordToTest[] = "test";
+        strcpy(passwordToTest, "");
+        int val = i;
+        for (int j = 0; j < password_max_length; j++)
+        {
+            int ch = val % number_of_characters;
+            passwordToTest[j] = characters[ch];
+            val = val / number_of_characters;    
+                           
+        }
         hash(passwordToTest, buffer);
         if(verbose) 
         {
@@ -23,7 +35,7 @@ int bruteforce_crack(char* password_hash, char * characters, int length, int ver
         {
             printf("Password found: %s\n", passwordToTest);
             return 0;
-        }        
+        }       
     }
     return 1;
 }
--- cli.c
+++ cli.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
     int verbose_flag = 0;
     int bruteforce_flag = 0;
     int dictionary_flag = 0;
-    int character_length = 4;
+    int character_length = 3;
     char password_hash[64] = "";
     char bruteforce_characters[256] = "";
     char dictionary_path[256] = "";
