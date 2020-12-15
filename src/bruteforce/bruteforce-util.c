d19a197c0185a54c59ca45a30e757a1de9708703
ma.luo <james.luo.3131@gmail.com>
2020-12-14 23:09:58 -0500

    make comments on will's extracted functions

--- src/bruteforce/bruteforce-util.c
+++ src/bruteforce/bruteforce-util.c
@@ -6,113 +6,17 @@
 #include "bruteforce.h"
 
 /*
-* @Author: Ma Luo
-* 
-* @passwordToTest - password to test with, values edits would be effective in the memory
-* @characters - character set to compare with
-* @pass_idx - password index
-* @buf_length - length of the character set, since we have it in the original call, no need to add more space complexity here
-* @next_idx - help decide next val to assign into passwordToTest
+* @Author: Willam Anderson
+*
+* This function is used to generate probabilities for specific password lengh and print it out when the rank encounters 0 
+*
+* @number_of_characters - size of pattern set to match the hashed password
+* @length - value varies from 0 to maxium password lengh (non-hashed one)
+* @verbose - flag for showing debugging message
+* @my_rank - rank of process 0 to p
+* return possibilities - number of probabilities with password lengh - @length
 *
 */
-
-int assignCharInBuffer(char *passwordToTest, char *characters, int pass_idx, int buf_length, int next_idx);
-
-/*
-* @Author: Ma Luo
-* 
-* @passhash - hashed password with sha256 values
-* @buf - character set to compare with
-* @pass - actual password we test with
-* @return result - 1 (not found)/ 0 (found)
-*/
-
-int findPasswordOrNo(char *passhash, char *buf, char *pass);
-
-/*
-* @Author: Ma Luo
-* 
-* @base - starting point of a number set (smallest)
-* @max - maxium value of a set of numbers
-* @step - within each step, we are going to increase base by one step untill it reach the maxium value
-* @return nextStep - return base + step or keep the maxium value as the ending point
-*/
-
-int climbToMax(int base, int max, int step);
-
-/*
-* @Author: Ma Luo
-* 
-* @pass - matched password if we have found it
-* @check - either 1 / 0, please look at the micro value for PASS_FOUND & PASS_NOT_FOUND
-* 
-* PASS_FOUND 0
-* PASS_NOT_FOUND 1
-*
-*/
-
-void printPassIfFound(char *pass, int check);
-
-void Greetings(char *password_hash, int password_max_length, char *characters, int number_of_characters);
-
-int assignCharInBuffer(char *passwordToTest, char *characters, int pass_idx, int buf_length, int next_idx)
-{
-    passwordToTest[pass_idx] = characters[next_idx % buf_length];
-    next_idx = next_idx / buf_length;
-    return next_idx;
-}
-
-int findPasswordOrNo(char *passhash, char *buf, char *pass)
-{
-    int result = 1;
-    if (!strcmp(passhash, buf))
-    {
-        printf("Password found: %s\n", pass);
-        result = 0;
-    }
-    return result;
-}
-
-int climbToMax(int base, int max, int step)
-{
-    int nextStep;
-    if (base + step > max)
-    {
-        nextStep = max;
-    }
-    else
-    {
-        nextStep = base + step;
-    }
-    return nextStep;
-}
-
-void printPassIfFound(char *pass, int check)
-{
-    if (check == PASS_FOUND)
-    {
-        printf("Password found: %s\n", pass);
-    }
-    else if (check == PASS_NOT_FOUND)
-    {
-        printf("Password not found.\n");
-    }
-}
-
-void Greetings(char *password_hash, int password_max_length, char *characters, int number_of_characters)
-{
-    printf("Brute force of hash: %s\n", password_hash);
-    printf("Using %d characters: %s\n", number_of_characters, characters);
-    printf("Calculating to a length of %d\n", password_max_length);
-}
-
-void print_stats(char *password_hash, char *characters, int number_of_characters, int password_max_length)
-{
-    printf("Brute force of hash: %s\n", password_hash);
-    printf("Using %d characters: %s\n", number_of_characters, characters);
-    printf("Calculating to a length of %d\n", password_max_length);
-}
-
 long calculate_possibilities(int number_of_characters, int length, int verbose, int my_rank)
 {
     long possibilities = (long)pow(number_of_characters, length);
@@ -123,6 +27,19 @@ long calculate_possibilities(int number_of_characters, int length, int verbose, 
     return possibilities;
 }
 
+/*
+* @Author: Willam Anderson
+*
+* This function is used to give actual password in the result field.
+* To be notified, the operations on the result, will be effective in the device memory
+*
+* @length - size of password length (non-hashed)
+* @characters - character set to compare with
+* @number_of_characters - size of characters sets
+* @seed - index to the character set, and reflecting to password set with [seed % number_of_characters] in a recursion way in the loop
+* @result - buffer to hold the password to be tested with
+*
+*/
 void generate_password(int length, char *characters, int number_of_characters, int seed, char * result)
 {
     int i;
@@ -134,6 +51,19 @@ void generate_password(int length, char *characters, int number_of_characters, i
     result[length] = '\0';
 }
 
+
+/*
+* @Author: Willam Anderson
+*
+* This function is used to give actual password in the result field.
+* To be notified, the operations on the result, will be effective in the device memory
+*
+* @counter - base counter from last step
+* @possibilities - maxium num of the probalities
+* @chunk_size - step size, each step will increase counter by @chunk_size
+* return @counter + @chunk_size - returns the counter for next step
+*
+*/
 int calculate_next_step(int counter, int possibilities, int chunk_size)
 {
     if (counter + chunk_size > possibilities)
