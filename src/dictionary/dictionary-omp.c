5d26467ec3c79a7d2c0286c51c05bd3b28ef89de
Will Anderson <willstoney@gmail.com>
2020-12-16 12:07:13 -0500

    Break out OMP implementaiton to it's own fuction so it can be called twice.

--- src/dictionary/dictionary-omp.c
+++ src/dictionary/dictionary-omp.c
@@ -7,6 +7,7 @@
 #include "../globals.h"
 
 int compare_candidates(FILE **file, char *password_hash, int verbose);
+int run_chunk(char *password_hash, char **candidate_array, int chunk_size, int verbose);
 
 /**
  * dictionary_crack() - OpenMP Implementation
@@ -67,18 +68,45 @@ int compare_candidates(FILE **file, char *password_hash, int verbose)
         remove_new_line(line, &candidate_array[counter]);
         if (counter++ == CHUNK_SIZE)
         {
-            #pragma omp parallel
-            #pragma omp for schedule(auto)
-            for (counter=0; counter<CHUNK_SIZE; counter++) 
-            {
-                int tempResult = do_comparison(password_hash, candidate_array[counter], verbose);
-                if (tempResult == FOUND) {
-                    #pragma omp critical
-                        result = FOUND;
-                } 
-            }
-            counter = 0;
+            result = run_chunk(password_hash, candidate_array, CHUNK_SIZE, verbose);
+            counter=0;
         }
+    }
+    // finish off remaining work that didn't fit in a chunk.
+    if (counter > 0)
+    {
+        result = run_chunk(password_hash, candidate_array, counter, verbose);
+    }
+    return result;
+}
+
+/**
+ * run_chunk() - does hash comaprison on a chunk of passwords read from disk
+ * (OpenMP Implementation)
+ * 
+ * 1. Manages iterating through the dictionary file and initiating the has comparisons.
+ * 2. Returns the result value (FOUND or NOT_FOUND) and the plain text password, if found.
+ *
+ * @param file is a pointer to the dictionary file in memory.
+ * @param password_hash is the hashed password string to crack.
+ * @param candidate_array is the array of candidate passwords.
+ * @param array_size is the size of the candidate array.
+ * @param verbose is a flag for verbose mode. Set to 1 to enable.
+ * @return the result as an integer value, FOUND (0) or NOT_FOUND (1).
+ */
+int run_chunk(char *password_hash, char **candidate_array, int array_size, int verbose)
+{
+    int result = NOT_FOUND;
+    int i;
+    #pragma omp parallel
+    #pragma omp for schedule(auto)
+    for (i=0; i<array_size; i++) 
+    {
+        int tempResult = do_comparison(password_hash, candidate_array[i], verbose);
+        if (tempResult == FOUND) {
+            #pragma omp critical
+                result = FOUND;
+        } 
     } 
     return result;
 }
